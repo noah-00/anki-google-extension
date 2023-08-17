7fa250ba09ad974d0983007600cc1375c813caa3
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-08-17 21:20:12 +0900

    ✨ feature get deck from Anki

---
+++ src/utils/functions.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { ANKI_LOCAL_URL } from "./const";
+
+export const apiAnkiClient = axios.create({
+  baseURL: ANKI_LOCAL_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const setJsonToAnki = (
+  action: string,
+  version: number,
+  params: { [key: string]: any } = {}
+): string => {
+  return JSON.stringify({ action, version, params });
+};
